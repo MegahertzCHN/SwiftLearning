ea08bf25ee8727d69373536f0dc8d7b07e18d135
zhaohe <zhaohe@easyretailpro.com>
2020-06-03 14:36:52 +0800

    swiftproduct - init

---
+++ swiftProduct/swiftProduct/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftProduct
+//
+//  Created by megahertz on 2020/6/3.
+//  Copyright © 2020 megahertz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
